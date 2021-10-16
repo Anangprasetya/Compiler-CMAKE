b78045091a839b3d94299104d683ef5785999555
Anang <tyoandini@gmail.com>
2021-10-16 18:23:02 +0700

    Class Helper - console

---
+++ 2-ClassHelper/bagian-1.cpp
@@ -0,0 +1,18 @@
+#include <wx/string.h>
+
+int main(){
+	wxPuts("Ini adalah kalimat yang dicetak di konsol");
+}
+
+
+/*
+         CMake       : 3.16.3
+         wxWidgets   : 3.0.5
+         G++         : 9.3.0
+         Sumber Ilmu : https://sites.google.com/site/ajarprogramming/gui
+         Dokumentasi : https://www.wxwidgets.org/docs/tutorials/
+         Tahun       : 2021
+         Pembuat     : Anang Nur Prasetya
+         OS          : Linux Ubuntu
+         Materi      : Class Helper (Menampilkan di konsol)
+*/
